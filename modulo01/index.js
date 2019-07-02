65276d31f4877828d0d66590364811a4fc90210e
Rodrigo Gomes <rodgomesc@localhost.localdomain>
2019-07-02 17:03:21 -0300

    ambiente-e-conceitos/query-route-params

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'hello world' });
+// Query Params = ?teste=1
+// Route Params = /users/1
+// Request Body = {"name":"rodrigo", "email":"rodgomesc@gmail.com"}
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuário ${id}` });
 });
 
 server.listen(3000);
