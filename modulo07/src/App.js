94c26e4f8153a805a66116c76ee808b7d036d032
Rodrigo Gomes <rodgomesc@localhost.localdomain>
2019-07-10 17:41:00 -0300

    arquitetura-flux/estilos-globais

--- modulo07/src/App.js
+++ modulo07/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
       <div>Hello from Header</div>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
