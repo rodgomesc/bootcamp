5553dc5aac35e61af190efed7ac9d1f96ef7f827
Rodrigo Gomes <rodgomesc@localhost.localdomain>
2019-07-22 17:24:40 -0300

    arquitetura-flux/reactotron-saga

--- modulo07/src/store/index.js
+++ modulo07/src/store/index.js
@@ -3,7 +3,12 @@ import createSagaMiddleWare from 'redux-saga';
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMiddleWare = createSagaMiddleWare();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleWare = createSagaMiddleWare({ sagaMonitor });
 
 const enhancer =
   process.env.NODE_ENV === 'development'
