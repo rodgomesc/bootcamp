be57450562ea25d28dcb3569e2fdfbd8978f67a2
Rodrigo Gomes <rodgomesc@localhost.localdomain>
2019-07-22 17:14:48 -0300

    /arquitetura-flux//configurando-redux-saga

--- modulo07/src/store/index.js
+++ modulo07/src/store/index.js
@@ -1,9 +1,20 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleWare from 'redux-saga';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMiddleWare = createSagaMiddleWare();
 
 const enhancer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(sagaMiddleWare)
+      )
+    : applyMiddleware(sagaMiddleWare);
 
 const store = createStore(rootReducer, enhancer);
+
+sagaMiddleWare.run(rootSaga);
 
 export default store;
