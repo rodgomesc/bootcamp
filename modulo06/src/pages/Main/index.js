cc501e807b13582559530c4a3544d7218d439fb9
rodgomesc <rodgomesc@gmail.com>
2019-07-25 12:17:39 -0300

    primeiro-projeto-com-react-native/acessando-api-do-github

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return <View />;
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+    const response = await api.get(`/users/${newUser}`);
+
+    data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+  render() {
+    const { user, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar Usuário"
+            value={newUser} // isso vai permitir limpar o input quando newUser mudar para ''
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function User() {
-  return <View />;
+  return <Container />;
 }
