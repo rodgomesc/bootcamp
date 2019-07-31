1967015f215e62454f6cca528dfffc7cc15bd218
rodgomesc <rodgomesc@gmail.com>
2019-07-31 11:07:49 -0300

    iniciando-back-end-do-gobarber/configurando-sequelize

---
+++ modulo02/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '6267764726',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    undescored: true,
+    undescoredAll: true,
+  },
+};
