a8bb3ba9a07dc0cd36e3df945526e70de9cb3faa
rodgomesc <rodgomesc@gmail.com>
2019-07-30 22:32:40 -0300

    iniciando-back-end-do-gobarber/eslint-prettier-editorconfig

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -7,6 +7,7 @@ class App {
     this.middlewares();
     this.routes();
   }
+
   middlewares() {
     this.server.use(express.json());
   }
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'sucessfull' });
-});
+routes.get('/', (req, res) => res.json({ message: 'sucessfull' }));
 
 export default routes;
