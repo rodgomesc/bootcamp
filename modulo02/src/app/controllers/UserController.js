0f1fcaf01b7a55ab4e4a462b55cf0455a46e251f
rodgomesc <rodgomesc@gmail.com>
2019-08-01 14:01:25 -0300

    iniciando-back-end-do-gobarber/update-do-usuario

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -20,7 +20,29 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: res.userId });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(res.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+      if (userExists) {
+        return res.status(400).json({ message: 'User already exists!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password does not match!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      email,
+      name,
+      provider,
+    });
   }
 }
 
