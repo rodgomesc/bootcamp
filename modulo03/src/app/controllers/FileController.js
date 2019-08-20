ec3a2b6d01d626d811ea3558fd8af11224754406
Rodrigo Gomes <rodgomesc@gmail.com>
2019-08-20 17:48:08 -0300

    continuando-api-do-gobarber/listagem-de-prestadores-de-servico

--- modulo03/src/app/controllers/FileController.js
+++ modulo03/src/app/controllers/FileController.js
@@ -3,7 +3,6 @@ import File from '../models/File';
 class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
-    console.log(req.file);
 
     const file = await File.create({
       name,
--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -4,6 +4,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -15,6 +16,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
