48a67814ba7a8cb8ff8976220a0ec8fb0fc6ab33
Rodrigo Gomes <rodgomesc@gmail.com>
2019-08-30 17:14:47 -0300

    continuando-api-do-gobarber/lesson/tratamento-de-excecoes

--- modulo03/src/app.js
+++ modulo03/src/app.js
@@ -1,17 +1,28 @@
 import express from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import { resolve } from 'path';
 import routes from './routes';
 import './database';
 
+import sentryConfig from './config/sentry';
+
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
@@ -20,6 +31,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
