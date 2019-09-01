7a7607c6d3341e5fe1e79b734690d1dae6bef1de
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-01 18:55:07 -0300

    gobarber-web/utilizando-root-import

--- modulo09/.eslintrc.js
+++ modulo09/.eslintrc.js
@@ -21,5 +21,22 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react/jsx-props-no-spreading': 0,
+  },
+
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
   },
 };
