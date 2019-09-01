86ce76ca13375ff220532f3192750644f276e907
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-01 11:43:49 -0300

    gobarber-web/configurando-reactotron-1

--- modulo09/src/App.js
+++ modulo09/src/App.js
@@ -3,7 +3,9 @@ import { Router } from 'react-router-dom';
 // import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
+import './config/ReactotronConfig';
 
+console.tron('helloo world');
 function App() {
   return (
     <Router history={history}>
