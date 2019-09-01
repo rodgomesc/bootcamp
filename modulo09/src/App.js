5c4074ab6a107d95700eff5a0eea2be8e3c1203f
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-01 18:11:43 -0300

    gobarber-web/estilos-globais-2

--- modulo09/src/App.js
+++ modulo09/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-// import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
 import './config/ReactotronConfig';
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
