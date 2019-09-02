28e3b9db308e50122b7afd4aa04b2967ff60e582
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-02 20:44:23 -0300

    gobarber-web/configurando-store

--- modulo09/src/App.js
+++ modulo09/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 import history from './services/history';
 import './config/ReactotronConfig';
 
+import store from './store';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 
