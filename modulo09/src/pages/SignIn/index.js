e7833516f3693bef60821a36fa9e364348345b9f
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-02 15:50:54 -0300

    gobarber-web/lesson/utilizando-unform

--- modulo09/src/pages/SignIn/index.js
+++ modulo09/src/pages/SignIn/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Input, Form } from '@rocketseat/unform';
 import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber Logo" />
 
-      <form action="">
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Sua Senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua Senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
