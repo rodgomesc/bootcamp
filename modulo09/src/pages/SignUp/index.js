6d1f804e231d0b2298d987a9a9da677df27ba133
Rodrigo Gomes <rodgomesc@gmail.com>
2019-09-02 10:45:19 -0300

    gobarber-web/estilizacao-da-autenticacao

--- modulo09/src/pages/SignUp/index.js
+++ modulo09/src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber Logo" />
+
+      <form action="">
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Seu email" />
+        <input type="password" placeholder="Sua Senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já Tenho Login</Link>
+      </form>
+    </>
+  );
 }
