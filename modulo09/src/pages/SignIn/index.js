d68f90d9c61c3b3518361dca3ba0d82278df708e
Rodrigo <rodgomesc@gmail.com>
2019-09-05 13:56:38 -0300

    gobarber-web/loading-da-autenticacao

--- modulo09/src/pages/SignIn/index.js
+++ modulo09/src/pages/SignIn/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Input, Form } from '@rocketseat/unform';
 import * as Yup from 'yup';
 import logo from '~/assets/logo.svg';
@@ -16,6 +16,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -28,7 +29,7 @@ export default function SignIn() {
         <Input name="email" type="email" placeholder="Seu email" />
         <Input name="password" type="password" placeholder="Sua Senha" />
 
-        <button type="submit">Acessar</button>
+        <button type="submit">{loading ? 'Carregando' : 'Acessar'}</button>
         <Link to="/register">Criar conta gratuita</Link>
       </Form>
     </>
