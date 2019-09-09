4297de0108e6042037ab9ea36aa407e00e2408ed
Rodrigo <rodgomesc@gmail.com>
2019-09-09 20:06:33 -0300

    gobarber-web/pagina-de-perfil

--- modulo09/src/pages/Profile/index.js
+++ modulo09/src/pages/Profile/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha Atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme a Senha"
+        />
+        <button type="button">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
